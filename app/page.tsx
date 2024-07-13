cab1da962d00c6d3ec940ee8c30f3ee6a55ab5b1
Manasseh Oruebor <manassehoruebor@gmail.com>
2024-07-13 15:18:18 +0200

    working

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,83 @@
+"use client";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
-
+import girl from "../assets/momus.jpg";
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
+import Confetti from "react-confetti";
 export default function Home() {
+  const [openCard, setopenCard] = useState<boolean>(false);
+
+  const [showConfetti, setShowConfetti] = useState<boolean>(false);
+  const [dimensions, setDimensions] = useState({
+    width: 0,
+    height: 0,
+  });
+  useEffect(() => {
+    const { innerWidth: width, innerHeight: height } = window;
+    setDimensions({
+      width,
+      height,
+    });
+  }, []);
+
+  function doSomething() {
+    setShowConfetti((prevState) => true);
+  }
+
+  // Set the interval to 120000 milliseconds (2 minutes)
+  const intervalId = setInterval(doSomething, 2000);
+
+  // To stop the interval after a certain amount of time (e.g., 10 minutes)
+  // Uncomment the following lines:
+
+  setTimeout(() => {
+    clearInterval(intervalId);
+  }, 20000);
+  const toggleCard = () => {
+    setopenCard((prevState) => !prevState);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      {showConfetti && (
+        <Confetti width={dimensions.width} height={dimensions.height} />
+      )}
+      <div className={`${openCard ? "hoverCard" : null} birthdayCard`}>
+        <div className="cardFront">
+          <h3 className="happy p-2 text-xl flex justify-center items-center text-center">
+            <span className="mx-2"> HAPPY BIRTHDAY</span>{" "}
+            <AiOutlineArrowRight onClick={toggleCard} />{" "}
+          </h3>
+          <div className="balloons">
+            <div className="balloon-1"></div>
+            <div className="balloon-2"></div>
+
+            <section className="balloon-5">
+              <Image src={girl} alt="bithday" className="birthday-girl" />
+            </section>
+            <div className="balloon-3"></div>
+            <div className="balloon-4"></div>
+          </div>
+        </div>
+        <div className="cardInside p-5 flex flex-col text-xl text-black">
+          <h3 className="back text-xl flex justify-center items-center text-center">
+            <span className="mx-4"> HAPPY BIRTHDAY</span>{" "}
+            <AiOutlineArrowLeft onClick={() => setopenCard(false)} />
+          </h3>
+          <div className="flex p-5">
+            <h2>Dear Mummy U.S ,</h2>
+          </div>
+          <div className="p-5">
+            Happy 59th Birthday! On this special day, I want to express my
+            deepest gratitude for all the support and love you've given me over
+            the years. Your generosity, especially your financial help, has been
+            a true blessing, enabling me to pursue my dreams and overcome
+            challenges. I am incredibly fortunate to have you in my life, and I
+            hope this year brings you as much joy and happiness as you have
+            given me. May your birthday be filled with love, laughter, and
+            wonderful memories. <br /> <br /> Much love form your nephew OSE
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
